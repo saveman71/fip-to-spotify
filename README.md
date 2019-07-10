fce33bfd68aedfc11bfc3091231e3f4577ced31d
Antoine Bolvy <antoine.bolvy@gmail.com>
2019-07-10 15:37:20 +0200

    Get playlist id from args

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 This small script allows you to quickly fetch the current playing song on FIP (main channel configured, change the `stationId` to get the other sub-channels).
 
-Of course, replace the playlist id with your own.
+# Usage
+
+```bash
+python main.py <username> <playlist_id>
+```
--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import requests
 import spotipy
 import spotipy.util as util
 
-playlist_id = '7saBQuEumSFY5Mpp8vfr2h'
-scope = 'playlist-modify-private'
-
-fip_url = 'https://www.fip.fr/latest/api/graphql';
-
 if len(sys.argv) > 1:
     username = sys.argv[1]
 else:
     print("Usage: %s username" % (sys.argv[0],))
     sys.exit()
 
+scope = 'playlist-modify-private'
 token = util.prompt_for_user_token(username, scope)
 
 if token:
+    playlist_id = sys.argv[2]
+
+    fip_url = 'https://www.fip.fr/latest/api/graphql';
+
     params = dict(
         operationName='Now',
         variables='{"bannerPreset":"600x600-noTransform","stationId":7,"previousTrackLimit":3}',
