ae26e20a4ca8e90e50d6451e233adaa8ef267dce
Antoine Bolvy <antoine.bolvy@gmail.com>
2019-07-10 15:32:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Save current FIP song to spotify playlist
+
+This small script allows you to quickly fetch the current playing song on FIP (main channel configured, change the `stationId` to get the other sub-channels).
+
+Of course, replace the playlist id with your own.
---
+++ main.py
@@ -0,0 +1,48 @@
+import sys
+import requests
+import spotipy
+import spotipy.util as util
+
+playlist_id = '7saBQuEumSFY5Mpp8vfr2h'
+scope = 'playlist-modify-private'
+
+fip_url = 'https://www.fip.fr/latest/api/graphql';
+
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print("Usage: %s username" % (sys.argv[0],))
+    sys.exit()
+
+token = util.prompt_for_user_token(username, scope)
+
+if token:
+    params = dict(
+        operationName='Now',
+        variables='{"bannerPreset":"600x600-noTransform","stationId":7,"previousTrackLimit":3}',
+        extensions='{"persistedQuery":{"version":1,"sha256Hash":"8a931c7d177ff69709a79f4c213bd2403f0c11836c560bc22da55628d8100df8"}}',
+    )
+    resp = requests.get(url=fip_url, params=params)
+    data = resp.json()
+
+    try:
+        song = data['data']['now']['song'];
+    except KeyError:
+        print(data)
+        raise RuntimeError('No song found');
+
+    if not song:
+        raise RuntimeError('No song found');
+
+    links = song['external_links'];
+
+    if not links['spotify']:
+        print(links)
+        raise RuntimeError('No spotify song found');
+
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    results = sp.user_playlist_add_tracks(username, playlist_id, ['spotify:track:' + links['spotify']['id']])
+    print(results)
+else:
+    print("Can't get token for", username)
